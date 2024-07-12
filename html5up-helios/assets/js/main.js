516739abd900d594bf0ec14a910956adb2fcc5b5
Angela <qyang40@jh.edu>
2024-07-12 01:21:21 -0400

    Attempt to render 3D model

--- html5up-helios/assets/js/main.js
+++ html5up-helios/assets/js/main.js
@@ -3,61 +3,8 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-// import './style.css';
-// import * as THREE from 'three';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-// let scene, camera, renderer;
 
-// function init() {
-//   scene = new THREE.Scene();
-
-//   scene.background = new THREE.Color(0x87ceeb);
-
-//   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-//   camera.position.set(0,-0.1,0); // Adjust the camera position
-
-//   renderer = new THREE.WebGLRenderer();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-//   document.body.appendChild(renderer.domElement);
-
-//   // Add ambient light
-//   const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-//   scene.add(ambientLight);
-
-//   // Add directional light
-//   const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-//   directionalLight.position.set(5, 5, 5);
-//   scene.add(directionalLight);
-
-//   const loader = new GLTFLoader();
-//   loader.load('foodtest.glb', function (gltf) {
-//     scene.add(gltf.scene);
-//     //camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-//   }, undefined, function (error) {
-//     console.error(error);
-//   });
-
-//   window.addEventListener('resize', onWindowResize, false);
-
-//   animate();
-// }
-
-// function onWindowResize() {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// }
-
-// function animate() {
-//   requestAnimationFrame(animate);
-
-//   scene.rotation.y += 0.005;
-//   renderer.render(scene, camera);
-// }
-
-// init();
 
 (function($) {
 
--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("Your mom")
-
-
-
-function changeText(){
-    const pTag = document.getElementById("first")
-    console.log(pTag.innerHTML)
-    pTag.innerText= "What happened?"
-}
